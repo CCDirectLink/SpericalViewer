48486420da347c84ccc65def72344ec9180b21b9
streetclaw <streetclaw@users.noreply.github.com>
2017-01-02 16:42:20 +0100

    storageController core

--- app/js/general/controller/storage.js
+++ app/js/general/controller/storage.js
@@ -1,4 +1,30 @@
+const dirdata = require('./js/general/controller/dirdata.js');
+
 function gameStorageController()
 {
-	this.cacheData = Array();
+
+	this.cacheData = Object();
+
+	var cacheData = this.cacheData;
+
+	// load storage data ------
+	fs.readdir(spo.env.path.storage, function(err, list) {
+
+		for (var index in list)
+		{
+			if (list[index] != ".DS_Store")
+			{
+				dirdata.stats(spo.env.path.storage + spo.env.path.seperator + list[index], function(err, statResult){
+					if ((typeof cacheData[list[index]] == 'undefined') || (cacheData[list[index]]  == null))
+					{
+						cacheData[list[index]] = Object();
+					}
+					cacheData[list[index]] = {size: statResult.size};
+				});
+			}
+		}
+
+	});
+	// ------------------------
+
 }
