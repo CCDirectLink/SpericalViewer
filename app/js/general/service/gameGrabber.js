94e59717cc2c0daea7d56d4a3884bfc98fd21de2
streetclaw <streetclaw@users.noreply.github.com>
2017-01-11 12:27:32 +0100

    fix: gamegrabber (wrong access)

--- app/js/general/service/gameGrabber.js
+++ app/js/general/service/gameGrabber.js
@@ -82,28 +82,29 @@ function gameGrabber(entryPath, containerId)
 
 					// data
 
-					fs.readFile(entryPath + dataPath + spo.env.path.seperator + "database.json", function (err, data) {
-						if (typeof data == 'undefined') return;
-							var databaseJson = JSON.parse(data);
+					fs.readFile(entryPath + dataPath + spo.env.path.seperator + "database.json", function (err, rawData) {
+						if (typeof rawData == 'undefined') return;
+							var databaseJson = JSON.parse(rawData);
+							console.log(databaseJson);
 							spo.gameData.addData(data.shortId, "database", databaseJson);
 					})
 
-					fs.readFile(entryPath + dataPath + spo.env.path.seperator + "global-settings.json", function (err, data) {
-						if (typeof data == 'undefined') return;
-							var globalSettingsJson = JSON.parse(data);
+					fs.readFile(entryPath + dataPath + spo.env.path.seperator + "global-settings.json", function (err, rawData) {
+						if (typeof rawData == 'undefined') return;
+							var globalSettingsJson = JSON.parse(rawData);
 							spo.gameData.addData(data.shortId, "globalSettings", globalSettingsJson);
 					})
 
-					fs.readFile(entryPath + dataPath + spo.env.path.seperator + "item-database.json", function (err, data) {
-						if (typeof data == 'undefined') return;
-							var itemsJson = JSON.parse(data);
+					fs.readFile(entryPath + dataPath + spo.env.path.seperator + "item-database.json", function (err, rawData) {
+						if (typeof rawData == 'undefined') return;
+							var itemsJson = JSON.parse(rawData);
 							spo.gameData.addData(data.shortId, "items", itemsJson.items);
 					})
 
 					// media
 
-					fs.readFile(entryPath + mediaPath + spo.env.path.seperator + "font" + spo.env.path.seperator + "icons-items.png", function (err, data) {
-						if (typeof data == 'undefined') return;
+					fs.readFile(entryPath + mediaPath + spo.env.path.seperator + "font" + spo.env.path.seperator + "icons-items.png", function (err, rawData) {
+						if (typeof rawData == 'undefined') return;
 
 						var iconSpecify = ["undef", "item-helm", "item-sword", "item-belt", "item-shoe", "item-items", "item-key", "item-trade"]
 						var iconSet = { dimension: {width: 14, height: 16, xpad: 1, ypad: 1}, column: 8, row: 5 };
@@ -115,14 +116,14 @@ function gameGrabber(entryPath, containerId)
 								var startX = columnIndex * (iconSet.dimension.width + iconSet.dimension.xpad);
 								var startY = rowIndex * (iconSet.dimension.height + iconSet.dimension.ypad);
 
-								spo.imageData.addImage("items", iconSpecify[columnIndex] + rowIndex, data, "png", startX, startY, iconSet.dimension.width, iconSet.dimension.height);
+								spo.imageData.addImage("items", iconSpecify[columnIndex] + rowIndex, rawData, "png", startX, startY, iconSet.dimension.width, iconSet.dimension.height);
 							}
 						}
 
 					})
 
-					fs.readFile(entryPath + mediaPath + spo.env.path.seperator + "gui" + spo.env.path.seperator + "menu.png", function (err, data) {
-						if (typeof data == 'undefined') return;
+					fs.readFile(entryPath + mediaPath + spo.env.path.seperator + "gui" + spo.env.path.seperator + "menu.png", function (err, rawData) {
+						if (typeof rawData == 'undefined') return;
 
 						var iconSpecify = ["hp", "attack", "defense", "focus", "elemHeat", "elemCold", "elemShock", "elemWave"]
 						var iconSet = { dimension: {width: 11, height: 11, xpad: 1}, column: 8, xstart: 33, ystart: 280 };
@@ -132,7 +133,7 @@ function gameGrabber(entryPath, containerId)
 							var startX = (columnIndex * (iconSet.dimension.width + iconSet.dimension.xpad)) + iconSet.xstart;
 							var startY = iconSet.ystart;
 
-							spo.imageData.addImage("items", iconSpecify[columnIndex], data, "png", startX, startY, iconSet.dimension.width, iconSet.dimension.height);
+							spo.imageData.addImage("items", iconSpecify[columnIndex], rawData, "png", startX, startY, iconSet.dimension.width, iconSet.dimension.height);
 						}
 
 					})
