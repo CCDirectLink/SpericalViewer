1b8bd423e1dd68187379bff57ce99acc266260b1
streetclaw <streetclaw@users.noreply.github.com>
2017-03-18 16:28:42 +0100

    fix: trim leading zeros

--- genVersion.ps1
+++ genVersion.ps1
@@ -4,18 +4,26 @@
 # current location
 
 
-$netModRev= $(git log --oneline | measure-object –line)."lines"
-$netModDesc=$(git describe --tags --long)
-$netModV=$netModDesc.split("-")[0]
-$netModHashL=$(git rev-parse HEAD)
-$netModHash=$netModHashL.Substring(0,8)
+$dataRev=$(git log --oneline | measure-object –line)."lines"
+$dataDesc=$(git describe --tags --long)
+$dataV=$dataDesc.split("-")[0]
+$dataHashL=$(git rev-parse HEAD)
+$dataHash=$dataHashL.Substring(0,8)
 
-$netModDateDay=$(git log -1 --date=format:'%-d' --pretty=format:%cd)
-$netModDateMonth=$(git log -1 --date=format:'%-m' --pretty=format:%cd)
-$netModDateYear=$(git log -1 --date=format:'%-Y' --pretty=format:%cd)
+$dateDay=$(git log -1 --date=format:'%d' --pretty=format:%cd)
+$dateMonth=$(git log -1 --date=format:'%m' --pretty=format:%cd)
+$dateYear=$(git log -1 --date=format:'%Y' --pretty=format:%cd)
 
-echo $("SpericalViewer " + $netModV + " - rev " + $netModRev)
-echo $($netModHashL + " / " + $netModHash + " from " + $netModDateYear + "-" + $netModDateMonth + "-" + $netModDateDay)
+# TRIM
+
+$dateDay=($dateDay).TrimStart('0')
+$dateMonth=($dateMonth).TrimStart('0')
+$dateYear=($dateYear).TrimStart('0')
+
+# ECHO
+
+echo $("SpericalViewer " + $dataV + " - rev " + $dataRev)
+echo $($dataHashL + " / " + $dataHash + " from " + $dateYear + "-" + $dateMonth + "-" + $dateDay)
 
 # JSON VERSION
 
@@ -30,10 +38,10 @@ if ( -not (Test-Path $genFolder) ) { mkdir $genFolder }
 cp -force $($templateFolder + "\" + $versionFile) $($genFolder + "\" + $versionFile)
 
 # include data
-(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_REV_PLACEHOLDER_\$_', $netModRev | set-content $($genFolder + "\" + $versionFile)
-(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_DATE_DAY_PLACEHOLDER_\$_', $netModDateDay | set-content $($genFolder + "\" + $versionFile)
-(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_DATE_MONTH_PLACEHOLDER_\$_', $netModDateMonth | set-content $($genFolder + "\" + $versionFile)
-(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_DATE_YEAR_PLACEHOLDER_\$_', $netModDateYear | set-content $($genFolder + "\" + $versionFile)
-(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_HASH_PLACEHOLDER_\$_', $("`"" + $netModHash + "`"") | set-content $($genFolder + "\" + $versionFile)
-(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_HASH_LONG_PLACEHOLDER_\$_', $("`"" + $netModHashL + "`"") | set-content $($genFolder + "\" + $versionFile)
-(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_VER_PLACEHOLDER_\$_', $("`"" + $netModV + "`"") | set-content $($genFolder + "\" + $versionFile)
+(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_REV_PLACEHOLDER_\$_', $dataRev | set-content $($genFolder + "\" + $versionFile)
+(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_DATE_DAY_PLACEHOLDER_\$_', $dateDay | set-content $($genFolder + "\" + $versionFile)
+(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_DATE_MONTH_PLACEHOLDER_\$_', $dateMonth | set-content $($genFolder + "\" + $versionFile)
+(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_DATE_YEAR_PLACEHOLDER_\$_', $dateYear | set-content $($genFolder + "\" + $versionFile)
+(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_HASH_PLACEHOLDER_\$_', $("`"" + $dataHash + "`"") | set-content $($genFolder + "\" + $versionFile)
+(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_HASH_LONG_PLACEHOLDER_\$_', $("`"" + $dataHashL + "`"") | set-content $($genFolder + "\" + $versionFile)
+(get-content $($genFolder + "\" + $versionFile)) -replace '_\$_GIT_VER_PLACEHOLDER_\$_', $("`"" + $dataV + "`"") | set-content $($genFolder + "\" + $versionFile)
