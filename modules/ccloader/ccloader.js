998913e6ef2dadbf7a67b1a5120638beaf157290
2767Mr <2767Mr@gmx.at>
2017-08-30 13:42:21 +0200

    Added CCLoader installation
    Added CCLoader installation
    Added executing CC for Windows

--- modules/ccloader/ccloader.js
+++ modules/ccloader/ccloader.js
@@ -1,5 +1,5 @@
 function CCLoader(){
-	const DOWNLOAD_LINK = "https://github.com/CCDirectLink/CCLoader/archive/master.zip";
+	const DOWNLOAD_LINK = "https://codeload.github.com/CCDirectLink/CCLoader/zip/master";
 	var location = {};
 	
 	this.initialize = function(){
@@ -17,20 +17,68 @@ function CCLoader(){
 		$("#ccloaderData").html(_getTable());
 	}
 	
+	this.install = function(id){
+		$('#install' + id).html("");
+		$('h2').html(globals.langData.getEntry("downloading"));
+		
+		var filename = "ccloader.zip" //Might have to be changed to be platform-independant
+		var file = fs.createWriteStream(filename); 
+		var request = https.get(DOWNLOAD_LINK, function(response) {
+			response.on('end', function(data){
+				file.close();
+				$('h2').html(globals.langData.getEntry("installing"));
+				_extract(filename, globals.gameData.versions[id].path.main + "..", function(){
+					$('h2').html("");
+					globals.ccloader.display();
+				});
+			});
+			response.on('data', function(data){
+				file.write(data, function(err){
+					if(err)
+						throw err;
+				});
+			});
+		});
+	}
+	
+	this.start = function(id){
+		exec(globals.gameData.versions[id].path.main + "../crosscode-beta.exe"); //TODO: Make platform-independant
+	}
+	
+	function _extract(file, unzipPath, cb){
+		fs.createReadStream(file)
+			.pipe(unzip.Parse())
+			.on('entry', function (entry) {
+				if(entry.type === "Directory"){
+					try{
+						fs.mkdirSync(unzipPath + entry.path.substr(15));
+					} catch(e) {}
+					entry.autodrain();
+				} else if(entry.type === "File") {
+					entry.pipe(fs.createWriteStream(unzipPath + entry.path.substr(15)));
+				} else {
+					entry.autodrain();
+				}
+			})
+			.on('close', function () {
+				if(cb)
+					cb(unzipPath);
+			});
+	}
 	function _getTable(){
-		var tableString = "<table><tr><th>" + globals.langData.getEntry("id") + "</th><th>" + globals.langData.getEntry("version") + "</th><th>" + globals.langData.getEntry("installed") + "</th></tr>";
+		var tableString = "<table><tr><th>" + globals.langData.getEntry("id") + "</th><th>" + globals.langData.getEntry("version") + "</th><th>" + globals.langData.getEntry("installed") + "</th><th>" + globals.langData.getEntry("start") + "</th></tr>";
 		
 		for(var id in globals.gameData.versions){
 			var game = globals.gameData.versions[id];
-			tableString += "<tr><td>" + id + "</td><td>" + game.version.string + "</td><td>";
+			tableString += "<tr><td>" + id + "</td><td>" + game.version.string + "</td><td id=\"install" + id + "\">";
 			
 			if(_isInstalled(game)){
 				tableString += globals.langData.getEntry("installed");
 			}else{
-				tableString += "</tr><tr>install"
+				tableString += "<button onclick=\"globals.ccloader.install('" + id + "')\">" + globals.langData.getEntry("install") + "</button>";
 			}
 			
-			tableString += "</td></tr>";
+			tableString += "</td><td><button onclick=\"globals.ccloader.start('" + id + "')\">" + globals.langData.getEntry("start") + "</button></td></tr>";
 		}
 		
 		tableString += "</table>";
