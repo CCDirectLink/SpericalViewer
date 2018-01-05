901019a7bfb0fb0e4009f7a225b98d369a492548
streetclaw <streetclaw@users.noreply.github.com>
2018-01-05 23:49:32 +0100

    savegame dummy

---
+++ modules/savegame/savegame.js
@@ -0,0 +1,34 @@
+function Savegame(){
+	var statusArray = [];
+
+	var langEntries = globals.module.getLangData();
+	
+	function initialize(){
+		globals.gameData.registerObserver(function(game, property, value) {
+		}, "version");
+	}
+
+	globals.module.on("langChanged", function(id, subId, data) {
+		langEntries = data;
+	});
+	
+	this.display = function(){
+		$("#foundSaves").html(langEntries.content['savegame.foundSaves']);
+		$("h1").html(langEntries.content['savegame.savegames']);
+
+		// init table
+		$("#foundSavesContainer").html("<table>" + "" + "</table>");
+	}
+	
+	initialize();
+}
+
+globals.module.sharedMemory['savegame'] = {
+	controller: new Savegame()
+};
+
+globals.module.on("modulesLoaded", function(){
+	var id = globals.menu.add("Savegame", function(){}, "../modules/savegame/savegame.html", true, 0);
+	globals.menu.updateAll();
+	globals.menu.select(id);
+});
