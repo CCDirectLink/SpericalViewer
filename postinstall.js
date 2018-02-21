f98f8698823a146d6995de6fc1c7b97394942ba5
streetclaw <streetclaw@users.noreply.github.com>
2018-02-21 16:44:34 +0100

    electron-builder (install-app-deps) removed

--- postinstall.js
+++ postinstall.js
@@ -1,30 +1,6 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-var builderPath = "";
-
-if (process.platform == "darwin") {
-	console.log("Platform: darwin");
-	builderPath = `${__dirname}/node_modules/.bin/electron-builder`;
-}
-else if (process.platform == "win32") {
-	console.log("Platform: win");
-	builderPath = `${__dirname}/node_modules/.bin/electron-builder.cmd`;
-}
-else if (process.platform == "linux") {
-	console.log("Platform: linux");
-	builderPath = `${__dirname}/node_modules/.bin/electron-builder`;
-}
-else {
-	alert("Unknown System Environment\r\nPostinstall abort");
-}
-
-const builder = exec(builderPath + " install-app-deps", {"cwd": `${__dirname}`});
-
-builder.stdout.on('data', (data) => {
-  	console.log(`${data}`);
-});
-
 const genVersion = exec(`node genVersion.js`, {"cwd": `${__dirname}`});
 
 genVersion.stdout.on('data', (data) => {
