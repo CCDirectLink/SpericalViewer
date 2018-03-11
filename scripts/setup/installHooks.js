28126227cf09b486a3105907c9fcf456d4845640
streetclaw <streetclaw@users.noreply.github.com>
2018-03-11 16:18:06 +0100

    move postinstall into scripts

---
+++ scripts/setup/installHooks.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var runInit = false;
+
+process.argv.forEach((val, index) => {
+	if (val === 'init') {
+		runInit = true;
+	}
+});
+
+process.argv = [];
+
+// imports
+// const fs = require('fs');
+
+/** Execute postinstall
+  */
+exports.init = function() {
+};
+
+// function gitInstalled() {
+//
+// }
+
+// function gitFlowInstalled() {
+//
+// }
+
+if (runInit) {
+	exports.init();
+}
---
+++ scripts/setup/postinstall.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var runInit = false;
+
+process.argv.forEach((val, index) => {
+	if (val === 'init') {
+		runInit = true;
+	}
+});
+
+process.argv = [];
+
+// imports
+const installHooks = require('./installHooks');
+
+/** Execute postinstall
+  */
+exports.init = function() {
+	installHooks.init();
+};
+
+if (runInit) {
+	exports.init();
+}
