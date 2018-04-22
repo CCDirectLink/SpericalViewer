7efada8ecd72e68050db6bf81ed29cc0e7893b6c
2767Mr <2767mr@gmx.at>
2018-04-22 15:53:30 +0200

    Started working on savegame

--- modules/savegame/savegame.js
+++ modules/savegame/savegame.js
@@ -2,13 +2,16 @@
 'use strict';
 
 function Savegame() {
-	// var statusArray = [];
+	const savesArray = [];
 
 	let langEntries = globals.module.getLangData();
 
 	function initialize() {
-		globals.gameData.registerObserver(function(game, property, value) {},
-			'version');
+		globals.gameData.registerObserver(function(game, property, value) {
+			if (value) {
+				_lookForSave(game.shortId);
+			}
+		}.bind(this), 'version');
 	}
 
 	globals.module.on('langChanged', function(id, subId, data) {
@@ -21,8 +24,38 @@ function Savegame() {
 		$('h1').html(langEntries.content['savegame.savegames']);
 
 		// init table
-		$('#foundSavesContainer').html('<table>' + '' + '</table>');
+		$('#foundSavesContainer').html('<table>' + _getTable() + '</table>');
 	};
+
+	function _lookForSave(version) {
+		let file = path.join(globalData.versionList[version], '..', 'cc.save');
+		if(fs.existsSync(file)) {
+			fs.readFile(file, 'utf8', function(err, data) {
+				if(err)
+					throw err;
+					
+				savesArray[version] = JSON.parse(data);
+				globals.module.sharedMemory['savegame'].controller.display();
+			});
+		}
+	}
+
+	function _getTable(){
+		let tableString = '<tr><th>' +
+			langEntries.content['savegame.savegame'] +
+			'</th><th></th></tr>';
+
+		if (savesArray.length === 0) {
+			return langEntries.content['status.noGames'];
+		}
+
+		for (let i in savesArray) {
+			tableString += '<tr><td>' + savesArray[i].version + '</td><td><!-- Display edit button here --></td></tr>';
+		}
+
+		return tableString;
+	}
+
 
 	initialize();
 }
